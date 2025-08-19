a7681db03de9c03efa25b620e944cb4d3f370748
itwasntmeoritwas <tkonskiy@gmail.com>
2025-08-19 14:07:11 +0300

    Create env-debug.js

---
+++ api/api/env-debug.js
@@ -0,0 +1,10 @@
+// api/env-debug.js
+module.exports = async (req, res) => {
+  // Do NOT print the key, just whether it exists and its length
+  const val = process.env.OPENAI_API_KEY || "";
+  res.setHeader("Content-Type", "application/json");
+  res.status(200).end(JSON.stringify({
+    hasKey: Boolean(val),
+    length: val.length
+  }));
+};
